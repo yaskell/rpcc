2d40f69019ab1b35e63ab1c25755abce66d9beb3
Yaskell <yaskell.dev@gmail.com>
2024-12-03 20:09:30 +0100

    read from input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::env;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: <path> [--lex | --parse | --codegen]");
+        process::exit(1);
+    }
+
+    let path = &args[1];
+
+    if args.len() > 2 {
+        let flag = &args[2];
+
+        match flag.as_str() {
+            "--lex" => println!("Lexing"),
+            "--parse" => println!("Parsing"),
+            "--codegen" => println!("Code Generation"),
+            _ => eprintln!("Unknown flag: {}", flag),
+        }
+
+        println!("Path: {}, Flag: {}", path, flag);
+    } else {
+        println!("Path: {}", path);
+    }
+
+    process::exit(0);
 }
+
