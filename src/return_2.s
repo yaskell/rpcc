98faf54632c80768ef1e75a4d16d966fb3464827
Yaskell <yaskell.dev@gmail.com>
2024-12-03 20:09:17 +0100

    test with return2.c file

---
+++ src/return_2.s
@@ -0,0 +1,10 @@
+	.file	"return_2.c"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+	movl	$2, %eax
+	ret
+	.size	main, .-main
+	.ident	"GCC: (GNU) 13.2.0"
+	.section	.note.GNU-stack,"",@progbits
