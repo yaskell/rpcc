34534cb925ac2653f7057d7600b57d9afcb8fdaf
Yaskell <yaskell.dev@gmail.com>
2024-12-19 11:31:18 +0100

    Refactor argument parsing: Add structured flag handling

--- src/main.rs
+++ src/main.rs
@@ -3,45 +3,75 @@ use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Arguments::new(&args);
 
     match config.flag {
-        Some(x) => println!("Using file '{}' with flag '{}'", config.file_path, x),
+        Some(flag) => match flag {
+            Flag::Lex => lex(), 
+            Flag::Parse => parse(), 
+            Flag::Codegen => codegen(),
+            Flag::Assembly => assembly(),
+        }
         None => println!("Using file '{}'", config.file_path),
     }
 
     process::exit(0);
 }
 
-struct Config {
-    file_path: String,
-    flag: Option<String>,
+fn lex() {
+    todo!();
 }
 
-impl Config {
-    fn new(args: &[String]) -> Config {
+fn parse() {
+    todo!();
+}
+
+fn codegen() {
+    todo!();
+}
+
+fn assembly() {
+    todo!();
+}
+
+
+struct Arguments {
+    file_path: String,
+    flag: Option<Flag>,
+}
+
+enum Flag {
+    Lex,
+    Parse,
+    Codegen,
+    Assembly,
+}
+
+impl Arguments {
+    fn new(args: &[String]) -> Arguments {
 
         if args.len() < 2 {
             eprintln!("ERROR: not enough arguments");
-            eprintln!("Usage: <path> [--lex | --parse | --codegen]");
+            usage_message();
             process::exit(1);
         }
 
         if args.len() > 3 {
             eprintln!("ERROR: too many arguments");
-            eprintln!("Usage: <path> [--lex | --parse | --codegen]");
+            usage_message();
             process::exit(1)
         }
 
         let file_path = args[1].clone();
-        let flag: Option<String> = if args.len() == 3 {
+        let flag: Option<Flag> = if args.len() == 3 {
             match args[2].as_str() {
-                "--lex" => Some(String::from("lex")),
-                "--parse" => Some(String::from("parse")),
-                "--codegen" => Some(String::from("codegen")),
+                "--lex" => Some(Flag::Lex),
+                "--parse" => Some(Flag::Parse),
+                "--codegen" => Some(Flag::Codegen),
+                "-S" => Some(Flag::Assembly),
                 _ => {
                     eprintln!("ERROR: flag `{}` not recognized", args[2].as_str());
-                    eprintln!("Usage: <path> [--lex | --parse | --codegen]");
+                    usage_message();
                     process::exit(1);
                 }
             }
@@ -49,7 +79,11 @@ impl Config {
             None
         };
 
-        Config { file_path, flag }
+        Arguments { file_path, flag }
     }
+}
+
+fn usage_message() {
+    eprintln!("Usage: <path> [--lex | --parse | --codegen | -S]");
 }
 
