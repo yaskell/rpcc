a56b64a3858f501d249535d2764958449126125d
Yaskell <yaskell.dev@gmail.com>
2025-09-24 23:13:18 +0200

    add: generate asm ast

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,15 @@ mod lexer;
 mod token;
 mod ast;
 mod parser;
+mod code_emission;
 
 use lexer::lex;
-use parser::parse;
 
 use std::process;
 use std::fs;
 use std::env;
 
+use crate::code_emission::translate_program;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -35,8 +36,9 @@ fn main() {
     println!("Lexed file contents: {:?}", &lexed_val);
     if stop_before_parsing { println!("Stopped before parsing"); process::exit(0); }
     let parsed_val = parser::parse(&mut lexed_val.clone());
-    println!("Parsed file contents: {:?}", parsed_val);
+    println!("Parsed file contents: {:?}", &parsed_val);
     if stop_before_assembly_generation { println!("Stopped before generating assembly"); process::exit(0); }
+    println!("Assembly Generation: {:?}", translate_program(parsed_val));
     if stop_before_code_emission { println!("Stopped before code emission"); process::exit(0); }
     if emit_assembly_file { todo!() }
     process::exit(0);
