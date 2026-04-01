44b1f073b8bd745bfc29bf0fefa3c2ad846649d7
Yaskell <yaskell.dev@gmail.com>
2026-04-02 00:35:42 +0200

    refactor: switch to flake.nix instead of shell.nix

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Crust: Compiler for a large subset of C written in Rust";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        gcc
+        gdb
+        python315
+      ];
+    };
+  };
+}
