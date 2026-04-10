9cb0fbb1d3a855854c0cb6bde3a3f8fb90c11519
Yaskell <yaskell.dev@gmail.com>
2026-04-10 18:29:29 +0200

    add rust toolkit to flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Crust: Compiler for a large subset of C written in Rust";
+  description = "Crust: Toy compiler for a subset of C written in rust";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
@@ -17,10 +17,15 @@
         gcc
         gdb
         python315
+        cargo
+        rustc
+        rustfmt
+        clippy
+        rust-analyzer
       ];
-
       shellHook = ''
-        export PATH=$PATH:./target/release
+        export RUST_SRC_PATH=${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}
+        export PATH=$PATH:$(pwd)/target/release
       '';
     };
   };
