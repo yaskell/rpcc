74e6097bdeaead29536d78709d0bb9a629561f18
Yaskell <yaskell.dev@gmail.com>
2026-07-16 16:07:12 +0200

    add: replace invalid mov instructions to use RD10 as scratch register

--- src/asm/fix_instruction.rs
+++ src/asm/fix_instruction.rs
@@ -14,12 +14,29 @@ pub fn fix_function(function: asm::Function, offset: i32) -> asm::Function {
                 function
                     .instructions
                     .into_iter()
-                    .map(|instruction| fix_instruction(instruction)),
+                    .flat_map(|instruction| fix_instruction(instruction)),
             )
             .collect(),
     }
 }
 
-pub fn fix_instruction(instruction: asm::Instruction) -> asm::Instruction {
-    return instruction;
+pub fn fix_instruction(instruction: asm::Instruction) -> Vec<asm::Instruction> {
+    if let asm::Instruction::Move { src, dst } = instruction {
+        if let (asm::Operand::Stack(_), asm::Operand::Stack(_)) = (&src, &dst) {
+            vec![
+                asm::Instruction::Move {
+                    src,
+                    dst: asm::Operand::Register(asm::Register::R10),
+                },
+                asm::Instruction::Move {
+                    src: asm::Operand::Register(asm::Register::R10),
+                    dst,
+                },
+            ]
+        } else {
+            vec![asm::Instruction::Move { src, dst }]
+        }
+    } else {
+        vec![instruction]
+    }
 }
