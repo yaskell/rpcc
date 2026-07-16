9f0cbaf5c6a6ed7142edf68736d8f96e22e26114
Yaskell <yaskell.dev@gmail.com>
2026-07-16 15:01:55 +0200

    refactor: translate_unary_op signature

--- src/asm/translate.rs
+++ src/asm/translate.rs
@@ -15,8 +15,8 @@ fn translate_function(function: tacky::Function) -> asm::Function {
 
 fn translate_instruction(instructions: Vec<tacky::Instruction>) -> Vec<asm::Instruction> {
     let mut asm_instructions = Vec::new();
-    for val in instructions.iter() {
-        match val {
+    for instruction in instructions {
+        match instruction {
             tacky::Instruction::Return(val) => {
                 asm_instructions.push(asm::Instruction::Move {
                     src: translate_val(val),
@@ -27,7 +27,7 @@ fn translate_instruction(instructions: Vec<tacky::Instruction>) -> Vec<asm::Inst
             tacky::Instruction::Unary { op, src, dst } => {
                 asm_instructions.push(asm::Instruction::Move {
                     src: translate_val(src),
-                    dst: translate_val(dst),
+                    dst: translate_val(dst.clone()),
                 });
                 asm_instructions.push(asm::Instruction::Unary {
                     op: translate_unary_op(op),
@@ -39,15 +39,14 @@ fn translate_instruction(instructions: Vec<tacky::Instruction>) -> Vec<asm::Inst
     asm_instructions
 }
 
-//FIXME
-fn translate_val(val: &tacky::Val) -> asm::Operand {
+fn translate_val(val: tacky::Val) -> asm::Operand {
     match val {
-        tacky::Val::Constant(c) => asm::Operand::Imm(c.clone()),
-        tacky::Val::Var(v) => asm::Operand::Pseudo(v.clone()),
+        tacky::Val::Constant(c) => asm::Operand::Imm(c),
+        tacky::Val::Var(v) => asm::Operand::Pseudo(v),
     }
 }
 
-fn translate_unary_op(unary_op: &tacky::UnaryOp) -> asm::UnaryOp {
+fn translate_unary_op(unary_op: tacky::UnaryOp) -> asm::UnaryOp {
     match unary_op {
         tacky::UnaryOp::Complement => asm::UnaryOp::Not,
         tacky::UnaryOp::Negate => asm::UnaryOp::Neg,
