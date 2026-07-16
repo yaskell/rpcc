51f3cd6840b38d666168e8bdafb63dc24438773e
Yaskell <yaskell.dev@gmail.com>
2026-07-16 15:03:41 +0200

    fix: distributing of temporary variables across statements

--- src/tacky.rs
+++ src/tacky.rs
@@ -3,6 +3,16 @@ use crate::parser;
 type Identifier = String;
 type Int = i32;
 
+pub struct TemporaryVarAllocator {
+    count: i32,
+}
+
+impl TemporaryVarAllocator {
+    fn new() -> TemporaryVarAllocator {
+        TemporaryVarAllocator { count: 0 }
+    }
+}
+
 #[derive(Debug)]
 pub struct Program {
     pub function: Function,
@@ -33,15 +43,16 @@ pub enum UnaryOp {
 }
 
 pub fn translate_program(program: parser::Program) -> Program {
+    let mut tva = TemporaryVarAllocator::new();
     Program {
-        function: translate_function(program.function),
+        function: translate_function(program.function, &mut tva),
     }
 }
 
-fn translate_function(function: parser::Function) -> Function {
+fn translate_function(function: parser::Function, tva: &mut TemporaryVarAllocator) -> Function {
     Function {
         identifier: translate_identifier(function.name),
-        body: translate_statement(function.body),
+        body: translate_statement(function.body, tva),
     }
 }
 
@@ -49,12 +60,14 @@ fn translate_identifier(identifier: Identifier) -> String {
     identifier
 }
 
-fn translate_statement(statement: parser::Statement) -> Vec<Instruction> {
+fn translate_statement(
+    statement: parser::Statement,
+    tva: &mut TemporaryVarAllocator,
+) -> Vec<Instruction> {
     let mut instructions = Vec::new();
     match statement {
         parser::Statement::Return(expression) => {
-            //FIXME: count will reset on new statement
-            let value = translate_expression(expression, &mut instructions, 0);
+            let value = translate_expression(expression, &mut instructions, tva);
             instructions.push(Instruction::Return(value))
         }
     };
@@ -64,13 +77,16 @@ fn translate_statement(statement: parser::Statement) -> Vec<Instruction> {
 fn translate_expression(
     expression: parser::Expression,
     instructions: &mut Vec<Instruction>,
-    i: i32,
+    tva: &mut TemporaryVarAllocator,
 ) -> Val {
     match expression {
-        parser::Expression::Constant(int) => return Val::Constant(int),
+        parser::Expression::Constant(int) => Val::Constant(int),
         parser::Expression::Unary { operator, operand } => {
-            let src = translate_expression(*operand, instructions, i + 1);
-            let dst = Val::Var(format!("tmp.{i}").to_string());
+            let dst = Val::Var(format!("tmp.{}", tva.count).to_string());
+            let src = {
+                tva.count += 1;
+                translate_expression(*operand, instructions, tva)
+            };
 
             match operator {
                 parser::UnaryOp::Complement => instructions.push(Instruction::Unary {
@@ -84,7 +100,7 @@ fn translate_expression(
                     dst: dst.clone(),
                 }),
             }
-            return dst;
+            dst
         }
     }
 }
