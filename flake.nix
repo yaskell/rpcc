e8de06f908c8dc5f00a847960492fd1740960fb8
Yaskell <yaskell.dev@gmail.com>
2026-09-14 21:58:50 +0200

    refactor(flake): use packages instead of builtinputs and sort pkgs

--- flake.nix
+++ flake.nix
@@ -5,25 +5,23 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
+  outputs = {nixpkgs, ...}: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
   in {
     devShells.${system}.default = pkgs.mkShell {
-      buildInputs = with pkgs; [
+      packages = with pkgs; [
+        bacon
+        cargo
+        clippy
         gcc
         gdb
         python315 # Used for running test suite
-        cargo
+        rust-analyzer
         rustc
         rustfmt
-        clippy
-        bacon
-        rust-analyzer
       ];
+
       shellHook = ''
         export RUST_SRC_PATH=${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}
         export PATH=$PATH:$(pwd)/target/release
